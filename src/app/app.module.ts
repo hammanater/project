59c98b0f9feb85d082cc4f197b5577b5825e67a4
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-04 18:18:54 -0500

    Creating the Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { GamesComponent } from './games/games.component';
+import { GameListComponent } from './games/game-list/game-list.component';
+import { GameDetailsComponent } from './games/game-details/game-details.component';
+import { GameItemComponent } from './games/game-details/game-item/game-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    GamesComponent,
+    GameListComponent,
+    GameDetailsComponent,
+    GameItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
