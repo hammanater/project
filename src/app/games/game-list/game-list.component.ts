80a348a7133fa2bbc6648b087aaeaf4123525c3a
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-08 19:04:28 -0600

    Adding Content to the Games Components

--- src/app/games/game-list/game-list.component.ts
+++ src/app/games/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../games.model';
 
 @Component({
   selector: 'app-game-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
-  games = [];
+  games: Game[] = [
+    new Game('Star wars: Empire at War','A strategy game in which you play in the Imperial Civil War', 'https://www.gamers-outlet.net/image/cache/wp/gj/Covers/emp2_cover-500x500.webp')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
