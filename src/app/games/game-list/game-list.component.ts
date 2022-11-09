5515a3a057f98e2bc8def46a9f0e88b2414b3bfe
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-08 18:48:08 -0600

    Creating a "Games" model

--- src/app/games/game-list/game-list.component.ts
+++ src/app/games/game-list/game-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
-
+  games = [];
   constructor() { }
 
   ngOnInit(): void {
