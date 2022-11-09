9b91e75d5388ff2af03fddfcc6bb136d54197881
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-11-08 19:38:20 -0600

    Outputting a List of Games with ngFor

--- src/app/games/game-list/game-list.component.ts
+++ src/app/games/game-list/game-list.component.ts
@@ -8,8 +8,10 @@ import { Game } from '../games.model';
 })
 export class GameListComponent implements OnInit {
   games: Game[] = [
-    new Game('Star wars: Empire at War','A strategy game in which you play in the Imperial Civil War', 'https://www.gamers-outlet.net/image/cache/wp/gj/Covers/emp2_cover-500x500.webp')
-  ];
+    new Game('Star wars: Empire at War','A strategy game in which you play in the Imperial Civil War.', 'https://m.media-amazon.com/images/I/81vSTTOR7lL._SX342_.jpg'),
+    new Game('Sins of a Solar Empire 2','A recently announced sequal to the RTS game: Sins of a Solar Empire. Not much is known about the game yet.', 'https://assets-prd.ignimgs.com/2022/09/15/sins-of-a-solar-empire-2-button-1663273452419.jpg?width=300&crop=1%3A1%2Csmart'),
+    new Game('Stellaris','A Grand Strategy game that is set mainly for deplomacy, and space combat.', 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/Stellaris_cover_art.jpg/220px-Stellaris_cover_art.jpg')
+];
 
   constructor() { }
 
