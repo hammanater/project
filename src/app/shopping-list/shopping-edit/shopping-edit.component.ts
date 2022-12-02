d00baf4e233a12dd3fbfe1b925da485e19e37c45
hammanater <hammanbraden24@g-apps.monett.k12.mo.us>
2022-12-02 14:20:22 -0600

    Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter } from '@angular/core';
+import { Game } from 'src/app/games/games.model';
+import { Gms } from 'src/app/shared/gms.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('priceInput') priceInputRef: ElementRef;
+  gameAdded = new EventEmitter<Gms>();
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(){
+    const gmsName = this.nameInputRef.nativeElement.nameInput;
+    const gmsPrice = this.priceInputRef.nativeElement.priceInput;
+    const newGm = new Gms(gmsName, gmsPrice);
+    this.gameAdded.emit(newGm);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onGameAdded(Gm: Gms) {
+    this.Gm.push(Gm)
+  }
+
 }
